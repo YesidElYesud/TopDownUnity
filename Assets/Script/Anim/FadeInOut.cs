2ad61dbacb7d40cfc10a2b70594b080558aab9e2
YesidElYesud <yesidricardogp@gmail.com>
2025-06-14 08:41:42 -0500

    New TransicionEscenas

---
+++ Assets/Script/Anim/FadeInOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeInOut : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private AnimationClip AnimFade;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            StartCoroutine(CambiarEscena());
+        }
+    }
+
+    IEnumerator CambiarEscena()
+    {
+        animator.SetTrigger("Iniciar");
+
+        yield return new WaitForSeconds(AnimFade.length);
+
+        SceneManager.LoadScene(1); 
+    }
+}
