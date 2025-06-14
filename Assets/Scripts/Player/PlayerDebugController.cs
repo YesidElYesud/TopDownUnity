be130112c1f16364282c9ce81d109b2a422c7211
Danklif <josemanrenteria@gmail.com>
2025-06-14 11:46:11 -0500

    Added base damage system and UI

---
+++ Assets/Scripts/Player/PlayerDebugController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HealthComponent))]
+public class PlayerDebugController : MonoBehaviour
+{
+    private HealthComponent health;
+
+    void Start()
+    {
+        health = GetComponent<HealthComponent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Recibiendo 25 de daño tipo físico");
+            health.TakeDamage(new DamageInfo(25f, null, "physical"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Curando 20 de vida");
+            health.Heal(20f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Recargando 15 de escudo");
+            health.RechargeShield(15f);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PlayerHealthUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour
+{
+    public HealthComponent target;
+    public Image healthBar;
+    public Image shieldBar;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        healthBar.fillAmount = target.currentHealth / target.maxHealth;
+        shieldBar.fillAmount = target.currentShield / target.maxShield;
+    }
+}
