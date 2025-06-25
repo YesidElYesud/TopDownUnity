b2f848fac8392c9eed367a6b0250618ecdfd3455
YesidElYesud <yesidricardogp@gmail.com>
2025-06-25 15:26:22 -0500

    Update Anim Chip and Door

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    
+    private HashSet<string> keys = new HashSet<string>();
 
     public int monedas = 0;
     public TextMeshProUGUI monedasText;
@@ -36,11 +38,21 @@ public class GameManager : MonoBehaviour
         monedas += cantidad;
         UpdateMonedasUI();
     }
-    
+
+    public void addkey(string keyID)
+    {
+        keys.Add(keyID);
+        Debug.Log("Llave Obtenida" + keyID);
+    }
     void UpdateMonedasUI()
     {
         Debug.Log("Monedas:" + monedas);
         if (monedasText != null)
             monedasText.text = monedas.ToString();
+    }
+
+    public bool haskey(string keyID)
+    {
+        return keys.Contains(keyID);
     }
 }
---
+++ Assets/Scripts/Items/Puerta.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    public string idkeyneed;
+    private Animator animator;
+    private bool abierta = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!abierta && other.CompareTag("Player"))
+        {
+            if(GameManager.Instance.haskey(idkeyneed))
+            {
+                AbrirPuerta();
+            }
+        }
+    }
+
+    void AbrirPuerta()
+    {
+        abierta = true;
+        animator.SetTrigger("Abierta");
+    }
+}
